ee41dcd6a89291aa2f926b4bb7a412c7515129aa
soohyuneeee <soohyuneeee12@gmail.com>
2023-08-16 14:31:34 +0900

    {Add} setting

---
+++ src/test/java/com/mungpull/hearing/DdobagiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mungpull.hearing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DdobagiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
