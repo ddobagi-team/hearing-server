02368a81fb3f60f6b7160f2bab661404b2e2e650
soohyuneeee <soohyuneeee12@gmail.com>
2023-08-16 14:32:25 +0900

    {Add} setting

---
+++ src/main/java/com/mungpull/hearing/HearingApplication.java
@@ -0,0 +1,13 @@
+package com.mungpull.hearing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HearingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HearingApplication.class, args);
+	}
+
+}
